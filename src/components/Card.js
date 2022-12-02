7eef66dfc9f0ca0a7cfc171a5079923b905a76ed
Beatriz <bitrix24@hotmail.com>
2022-12-02 22:07:31 +0100

    working on responsiveness

--- src/components/Card.js
+++ src/components/Card.js
@@ -17,9 +17,12 @@ const Card = ( {book} )=>{
                             <>
                                 <div className="card group" 
                                 onClick={()=>{setShow(true);setItem(item)}}>
-                                    <img className="thumbnail mx-auto py-4 rounded-t-lg" src={thumbnail} alt={item.volumeInfo.title} />
+                                    <img className="thumbnail mx-auto max-[507px]:mx-6 h-56 py-4 rounded-t-lg" src={thumbnail} alt={item.volumeInfo.title} />
                                     <div className="card-bottom">
-                                        <h3 className="card-bottom__title">{item.volumeInfo.title}</h3>
+                                        <h3 className="card-bottom__title">{
+                                            item.volumeInfo.title.length > 36 ?
+                                            `${item.volumeInfo.title.substring(0, 36)}...` : item.volumeInfo.title
+                                        }</h3>
                                         <p className="card-bottom__amount">{amount}&#8364;</p>
                                     </div>
                                 </div>
